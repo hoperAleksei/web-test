d237a19735c9dd31426c03b52daaade4d6e999f9
Aleksei <85HopER85@gmail.com>
2023-01-20 17:41:15 +1000

    lolkek

--- lab4/lab_4_3/get_book.py
+++ lab4/lab_4_3/get_book.py
@@ -1,41 +1,43 @@
-from jinja2 import Template
-import sqlite3
-from book_model import get_publisher, get_author, get_genre, card
+if __name__ == '__main__':
 
-genres = tuple([1, 2, 3])
-authors = tuple([2, 3, 4, 5])
-publishers = tuple()
-conn = sqlite3.connect("library.sqlite")
+    from jinja2 import Template
+    import sqlite3
+    from book_model import get_publisher, get_author, get_genre, card
 
-with open("library.db", "r", encoding="utf-8-sig") as f:
-    damp = f.read()
-    conn.executescript(damp)
-    conn.commit()
+    genres = tuple()
+    authors = tuple()
+    publishers = tuple()
+    conn = sqlite3.connect("library.sqlite")
 
-df_author = get_author(conn)
-df_publisher = get_publisher(conn)
-df_genre = get_genre(conn)
-df_card = card(conn, publishers, genres, authors)
-conn.close()
+    with open("library.db", "r", encoding="utf-8-sig") as f:
+        damp = f.read()
+        conn.executescript(damp)
+        conn.commit()
 
-# f_template = open('template.html')
-file = open('book_templ.html', 'r', encoding='utf-8')
-# f_template.close()
-html = file.read()
-file.close()
+    df_author = get_author(conn)
+    df_publisher = get_publisher(conn)
+    df_genre = get_genre(conn)
+    df_card = card(conn, publishers, genres, authors)
+    conn.close()
 
-template = Template(html)
-result_html = template.render(
-    authors=df_author,
-    publishers=df_publisher,
-    genres=df_genre,
-    card=df_card,
-    sel_authors=authors,
-    sel_publishers=publishers,
-    sel_genres=genres,
-    len=len
-)
+    # f_template = open('template.html')
+    file = open('book_templ.html', 'r', encoding='utf-8')
+    # f_template.close()
+    html = file.read()
+    file.close()
 
-f = open('book.html', 'w', encoding='utf-8-sig')
-f.write(result_html)
-f.close()
+    template = Template(html)
+    result_html = template.render(
+        authors=df_author,
+        publishers=df_publisher,
+        genres=df_genre,
+        card=df_card,
+        sel_authors=authors,
+        sel_publishers=publishers,
+        sel_genres=genres,
+        len=len
+    )
+
+    f = open('book.html', 'w', encoding='utf-8-sig')
+    f.write(result_html)
+    f.close()
